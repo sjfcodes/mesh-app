8e5f24377f39d11470f919191e95fdd5dd02b85d
sjfox <samueljasonfox@gmail.com>
2022-12-29 01:19:15 -0800

    c-r-a-ts with aws cognito

---
+++ src/auth/config.ts
@@ -0,0 +1,18 @@
+const config = {
+  aws_cognito_region: "us-east-1", // (required) - Region where Amazon Cognito project was created
+  aws_user_pools_id: process.env.REACT_APP_USER_POOLS_ID, // (optional) -  Amazon Cognito User Pool ID
+  aws_user_pools_web_client_id: process.env.REACT_APP_USER_POOLS_WEB_CLIENT_ID, // (optional) - Amazon Cognito App Client ID (App client secret needs to be disabled)
+  aws_mandatory_sign_in: "enable", // (optional) - Users are not allowed to get the aws credentials unless they are signed in
+  ...(process.env.NODE_ENV !== "development"
+    ? {
+        cookieStorage: {
+          domain: process.env.REACT_APP_AUTH_COOKIE_DOMAIN,
+          path: "/",
+          expires: "365",
+          secure: true,
+        },
+      }
+    : {}),
+};
+
+export default config;
