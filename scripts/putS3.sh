158a6ff5da61bd36fb1d333e0b7c2ed75a2bf61e
sjfox <samueljasonfox@gmail.com>
2022-12-30 01:05:11 -0800

    update deploy scripts

--- scripts/putS3.sh
+++ scripts/putS3.sh
@@ -3,8 +3,7 @@ bucket="mesh-app" # the s3 bucket path
 
 aws s3 cp  --recursive "$build" "s3://$bucket" --profile mesh-app-deployer
 
-echo "\n deployment complete:  https://$bucket.net"
+echo "\nbrowse: https://$bucket.net"
 
-sleep 10
-
-open "https://$bucket.net"
+# sleep 10
+# open "https://$bucket.net"
