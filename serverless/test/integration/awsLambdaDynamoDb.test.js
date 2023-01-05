bd0dd8cc04abc14d7b444da794f5f0aa655f2006
sjfox <sjfcodes@gmail.com>
2023-01-05 07:33:47 -0800

    update integration test

--- serverless/test/integration/awsLambdaDynamoDb.test.js
+++ serverless/test/integration/awsLambdaDynamoDb.test.js
@@ -1,7 +1,22 @@
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import axios from "axios";
+
+import dynamoDb from "../../config/dynamoDb.mjs";
+import {
+  createTablePayload,
+  getTablePayload,
+  deleteTablePayload,
+} from "../../lambdas/crudDynamoDbTable/test/payloads.mjs";
+import {
+  createTableItemPayload,
+  deleteTableItemPayload,
+  getTableItemPayload,
+  updateTableItemPayload,
+} from "../../lambdas/crudDynamoDbTableItem/test/payloads.mjs";
 import {
   createTable,
-  getTable,
   deleteTable,
+  getTable,
 } from "../../lambdas/crudDynamoDbTable/test/modules.mjs";
 import {
   createTableItem,
@@ -10,21 +25,51 @@ import {
   updateTableItem,
 } from "../../lambdas/crudDynamoDbTableItem/test/modules.mjs";
 
-import dynamoDb from "../../config/dynamoDb.mjs";
+dotenv.config();
+
+const testApi = process.env.USE_API_GATEWAY === "true";
+console.log(`TESTING: ${testApi ? "AWS_API_GATEWAY" : "LOCAL"}`);
 
 const {
+  TableName,
   Item: { original, update },
 } = dynamoDb;
 
+const apiTable = axios.create({
+  baseURL: process.env.AWS_API_GATEWAY + "/dynamodbtable",
+  headers: { Authorization: process.env.AUTH_TOKEN },
+});
+
+const apiTableItem = axios.create({
+  baseURL: process.env.AWS_API_GATEWAY + "/dynamodbtableitem",
+  headers: { Authorization: process.env.AUTH_TOKEN },
+});
+
 describe("create and read table", () => {
   it("should create table", async () => {
-    const { statusCode, body } = await createTable();
+    const { statusCode, body } = await (testApi
+      ? apiTable({
+          method: createTablePayload.httpMethod,
+          data: createTablePayload,
+        }).then(({ data }) => data)
+      : createTable());
+
+    if (statusCode !== 200) console.error(body);
     await new Promise((resolve) => setTimeout(resolve, 10000));
+
     expect(statusCode).toBe(200);
+    expect(body.TableDescription.TableName).toBe(TableName);
   });
 
   it("should get table", async () => {
-    const { statusCode, body } = await getTable();
+    const { statusCode, body } = await (testApi
+      ? apiTable({
+          method: getTablePayload.httpMethod,
+          data: getTablePayload,
+        }).then(({ data }) => data)
+      : getTable());
+
+    if (statusCode !== 200) console.error(body);
 
     expect(statusCode).toBe(200);
   });
@@ -32,13 +77,27 @@ describe("create and read table", () => {
 
 describe("add, edit, & delete items from table", () => {
   it("should create Item", async () => {
-    const { statusCode, body } = await createTableItem();
+    const { statusCode, body } = await (testApi
+      ? apiTableItem({
+          method: createTableItemPayload.httpMethod,
+          data: createTableItemPayload,
+        }).then(({ data }) => data)
+      : createTableItem());
+
+    if (statusCode !== 200) console.error(body);
 
     expect(statusCode).toBe(200);
   });
 
   it("should get Item", async () => {
-    const { statusCode, body } = await getTableItem();
+    const { statusCode, body } = await (testApi
+      ? apiTableItem({
+          method: getTableItemPayload.httpMethod,
+          data: getTableItemPayload,
+        }).then(({ data }) => data)
+      : getTableItem());
+
+    if (statusCode !== 200) console.error(body);
 
     expect(statusCode).toBe(200);
     expect(body.Item.someData.S).toBe(original.someData.S);
@@ -48,13 +107,24 @@ describe("add, edit, & delete items from table", () => {
   });
 
   it("should update item", async () => {
-    const { statusCode, body } = await updateTableItem({
+    const updateExpression = {
       UpdateExpression: "SET verified = :v, someData = :sd",
       ExpressionAttributeValues: {
         ":v": update.verified,
         ":sd": update.someData,
       },
-    });
+    };
+    const payload = updateTableItemPayload(updateExpression);
+
+    const { statusCode, body } = await (testApi
+      ? apiTableItem({
+          method: payload.httpMethod,
+          data: payload,
+        }).then(({ data }) => data)
+      : updateTableItem(updateExpression));
+
+    if (statusCode !== 200) console.error(body);
+
     expect(statusCode).toBe(200);
     // expect unchanged
     expect(body.Attributes.email.S).toBe(original.email.S);
@@ -64,7 +134,14 @@ describe("add, edit, & delete items from table", () => {
   });
 
   it("should DELETE item from Table", async () => {
-    const { statusCode } = await deleteTableItem();
+    const { statusCode, body } = await (testApi
+      ? apiTableItem({
+          method: deleteTableItemPayload.httpMethod,
+          data: deleteTableItemPayload,
+        }).then(({ data }) => data)
+      : deleteTableItem());
+
+    if (statusCode !== 200) console.error(body);
 
     expect(statusCode).toBe(200);
   });
@@ -72,7 +149,15 @@ describe("add, edit, & delete items from table", () => {
 
 describe("destroy table", () => {
   it("should delete table", async () => {
-    const { statusCode, body } = await deleteTable();
+    const { statusCode, body } = await (testApi
+      ? apiTable({
+          method: deleteTablePayload.httpMethod,
+          data: deleteTablePayload,
+        }).then(({ data }) => data)
+      : deleteTable());
+
+    if (statusCode !== 200) console.error(body);
+
     expect(statusCode).toBe(200);
   });
 });
