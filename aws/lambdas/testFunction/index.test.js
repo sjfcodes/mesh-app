fe62b1d555bf4e67987c1077beb2c44c08263bd8
sjfox <samueljasonfox@gmail.com>
2023-01-04 19:39:23 -0800

    delete test functions

--- aws/lambdas/testFunction/index.test.js
+++
@@ -1,8 +0,0 @@
-import { handler } from "./index.mjs";
-
-describe("testFunction", () => {
-  it("should have body property", async () => {
-    const response = await handler({});
-    expect(response).toHaveProperty("body");
-  });
-});
