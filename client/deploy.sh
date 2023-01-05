34bfcd8f7fead1d3bd1a0ef8c54894ed9adc5850
sjfox <samueljasonfox@gmail.com>
2023-01-04 16:10:17 -0800

    update deploy script

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,9 +1,8 @@
-build=$1 # the path of the directory where the files and directories that need to be copied are located
+build="./build" # the path of the directory where the files and directories that need to be copied are located
 bucket="mesh-app" # the s3 bucket path
 
 aws s3 cp  --recursive "$build" "s3://$bucket" --profile mesh-app-deployer
 
 echo "\nbrowse: https://$bucket.net"
 
-# sleep 10
-# open "https://$bucket.net"
+rm -rf $build
