e3401ddf858cc955d291abd3b6eee033d1604a16
sjfox <sjfcodes@gmail.com>
2023-01-08 11:51:09 -0800

    update deploy all script, remove old test strategy

--- serverless/lambdas/deployAll.sh
+++ serverless/lambdas/deployAll.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
 npm run deploy:ddb-table
 npm run deploy:ddb-table-item
+npm run deploy:plaid-link
--- serverless/test/wip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export AWS_PROFILE=mesh-app-deployer 
-NODE_OPTIONS=--experimental-vm-modules npx jest lambdas/crudPlaidLink/index.test.js --watchAll
-
