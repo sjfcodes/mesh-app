7d02493afc6b8b5b597e6e744df64ae01ab7f104
sjfox <sjfcodes@gmail.com>
2023-01-14 14:36:57 -0800

    create AppButton

---
+++ client/src/components/Button/App/AppButton.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import './style.scss';
+
+type props = {
+  children: ReactNode;
+  label: string;
+  onClick: any;
+};
+
+const AppButton = ({ children, label, onClick }: props) => {
+  return (
+    <div className="ma-button-app">
+      <button onClick={onClick}>{children}</button>
+      <label>{label}</label>
+    </div>
+  );
+};
+
+export default AppButton;
