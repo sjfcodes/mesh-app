06206eb82af9d964c9a82bd7af9abef57d8b4b28
sjfox <sjfcodes@gmail.com>
2023-01-14 13:22:16 -0800

    link item count & account count to userCard data

--- client/src/components/UserCard/UserCard.tsx
+++ client/src/components/UserCard/UserCard.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import usePlaidItems from '../../hooks/usePlaidItems';
 
 import useUser from '../../hooks/useUser';
-import { pluralize } from '../../util';
 import AppLogo from '../AppLogo/AppLogo';
 
 import './styles.scss';
 
 export default function UserCard() {
   const { user } = useUser();
-  const [numOfItems] = useState(0);
+  const { plaidItem, allAccounts } = usePlaidItems();
+  const [itemCount, setItemCount] = useState({
+    items: 0,
+    accounts: allAccounts.length,
+  });
+
+  useEffect(() => {
+    setItemCount({
+      items: Object.keys(plaidItem).length,
+      accounts: allAccounts.length,
+    });
+  }, [plaidItem, allAccounts]);
 
   return (
     <div className="ma-user-card">
@@ -26,7 +37,11 @@ export default function UserCard() {
         </li>
         <li>
           <h3>connection</h3>
-          <p>{`{ ${pluralize('item', numOfItems)}: ${numOfItems} }`}</p>
+          <p>
+            {JSON.stringify(itemCount)
+              .replaceAll('"', ' ')
+              .replaceAll(':', ': ')}
+          </p>
         </li>
       </ul>
     </div>
