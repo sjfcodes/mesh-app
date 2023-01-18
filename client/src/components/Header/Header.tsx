eef9108980eb7b6dad2f0f9780836d5178ec0184
sjfox <sjfcodes@gmail.com>
2023-01-18 09:07:11 -0800

    add not on app/ url sync

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,13 +1,26 @@
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import UserCard from '../UserCard/UserCard';
 
 import './style.scss';
 
 const Header = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+  const handleClick = () => {
+    // navigate('/accounts');
+    /**
+     * navigate to accounts and reload does not work. 
+     * FooterNav contains local state responsible for
+     * keeping page url & selected app synced.
+     * 
+     * Would need to move local state to shared state
+     * to allow other components to set selected app/url.
+     */
+    window.location.reload();
+  };
+
   return (
-    <header onClick={() => navigate('/')}>
-      <div className="ma-header-left" onClick={() => window.location.reload()}>
+    <header onClick={handleClick}>
+      <div className="ma-header-left">
         <UserCard />
       </div>
       <h1>mesh</h1>
