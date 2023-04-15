778d67942e8fd559f00f8fb975b467ce9cd18110
sjfox <sjfcodes@gmail.com>
2023-04-14 17:13:43 -0700

    apply formatting

--- serverless/lambdas/crudPlaid/lib/App.mjs
+++ serverless/lambdas/crudPlaid/lib/App.mjs
@@ -130,7 +130,6 @@ class App {
       };
     }, {});
 
-
     return { account: formatted };
   }
 
--- serverless/lambdas/crudPlaid/utils/helpers.mjs
+++ serverless/lambdas/crudPlaid/utils/helpers.mjs
@@ -6,24 +6,24 @@
  * @returns
  */
 export const splitListIntoSmallerLists = (list, maxLength) => {
-    if (!list || !maxLength) throw new Error('missing required arguments!');
-    let requestQueue = [[]];
-    let pointer = 0;
-  
-    if (list.length === 0) {
-      return requestQueue;
-    }
-  
-    while (list.length) {
-      const nextRequest = list.pop();
+  if (!list || !maxLength) throw new Error('missing required arguments!');
+  let requestQueue = [[]];
+  let pointer = 0;
 
-      if (requestQueue[pointer].length === maxLength) {
-        requestQueue.push([nextRequest]);
-        pointer++;
-      } else {
-        requestQueue[pointer].push(nextRequest);
-      }
-    }
-  
+  if (list.length === 0) {
     return requestQueue;
-  };
+  }
+
+  while (list.length) {
+    const nextRequest = list.pop();
+
+    if (requestQueue[pointer].length === maxLength) {
+      requestQueue.push([nextRequest]);
+      pointer++;
+    } else {
+      requestQueue[pointer].push(nextRequest);
+    }
+  }
+
+  return requestQueue;
+};
