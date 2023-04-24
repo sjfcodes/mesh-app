03ced9bb966c74124895d36074396b5541f99e6c
sjfox <sjfcodes@gmail.com>
2023-04-23 21:17:29 -0700

    add service apigateway with cognito pool

--- serverless/terraform/services/apigateway/apigateway.tf
+++ serverless/terraform/services/apigateway/apigateway.tf
@@ -1,60 +1,3 @@
-resource "aws_api_gateway_rest_api" "this" {
-  body = jsonencode({
-    openapi = "3.0.1"
-    info = {
-      title   = "title_mesh-app"
-      version = "1.0"
-    }
-    paths = {
-      "/path1" = {
-        get = {
-          x-amazon-apigateway-integration = {
-            httpMethod           = "GET"
-            payloadFormatVersion = "1.0"
-            type                 = "HTTP_PROXY"
-            uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
-          }
-        }
-      }
-    }
-  })
-
-  name = "test_mesh-app"
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
-resource "aws_api_gateway_deployment" "this" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.this.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_stage" "test" {
-  deployment_id = aws_api_gateway_deployment.this.id
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  stage_name    = "test"
-}
-
-resource "aws_api_gateway_method_settings" "all" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  stage_name  = aws_api_gateway_stage.test.stage_name
-  method_path = "*/*"
-
-  settings {
-    metrics_enabled = true
-    logging_level   = "INFO"
-  }
-}
-
 variable "cognito_user_pool_name" {
   default = "mesh-app"
 }
@@ -63,10 +6,14 @@ data "aws_cognito_user_pools" "this" {
   name = var.cognito_user_pool_name
 }
 
+resource "aws_api_gateway_rest_api" "this" {
+  name = "test_mesh-app"
+}
+
 resource "aws_api_gateway_resource" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = "path1"
 }
 
 resource "aws_api_gateway_authorizer" "this" {
@@ -76,11 +23,22 @@ resource "aws_api_gateway_authorizer" "this" {
   provider_arns = data.aws_cognito_user_pools.this.arns
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/api_gateway_method
-resource "aws_api_gateway_method" "any" {
+resource "aws_api_gateway_method" "path1_get" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   resource_id   = aws_api_gateway_resource.this.id
-  http_method   = "ANY"
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.this.id
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_method" "path1_post" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.this.id
+  http_method   = "POST"
   authorization = "COGNITO_USER_POOLS"
   authorizer_id = aws_api_gateway_authorizer.this.id
 
