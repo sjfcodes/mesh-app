38697d496656418aab1c17e2faac9e0baf67a090
sjfox <sjfcodes@gmail.com>
2023-04-25 00:10:24 -0700

    add lambda outputs

--- serverless/terraform/services/lambdas/crudPlaid/crudPlaid.tf
+++ serverless/terraform/services/lambdas/crudPlaid/crudPlaid.tf
@@ -39,3 +39,10 @@ resource "aws_lambda_function" "test_lambda" {
     }
   }
 }
+
+output "function_name" {
+  value = aws_lambda_function.test_lambda.function_name
+}
+output "invoke_arn" {
+  value = aws_lambda_function.test_lambda.invoke_arn
+}
