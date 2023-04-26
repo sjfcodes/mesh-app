3647c81ad61b1f2de0e779c247023422207fa1c6
sjfox <sjfcodes@gmail.com>
2023-04-25 20:01:25 -0700

    update lambda service

--- serverless/terraform/services/lambdas/crudPlaid/crudPlaid.tf
+++ serverless/terraform/services/lambdas/crudPlaid/crudPlaid.tf
@@ -1,3 +1,7 @@
+variable "lambda_function_name" {
+  default = "crudPlaid"
+}
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -18,13 +22,13 @@ resource "aws_iam_role" "iam_role_lambda" {
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_dir = "${local.path_to_lambdas}/${local.lambda_crudPlaid}"
-  output_path = "${local.path_to_lambdas}/${local.lambda_crudPlaid}/${local.lambda_crudPlaid}.zip"
+  source_dir  = "${local.path_to_lambdas}/${var.lambda_function_name}"
+  output_path = "${local.path_to_lambdas}/${var.lambda_function_name}/${var.lambda_function_name}.zip"
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "${local.path_to_lambdas}/${local.lambda_crudPlaid}/${local.lambda_crudPlaid}.zip"
-  function_name = "${local.env}_${local.lambda_crudPlaid}"
+  filename      = "${local.path_to_lambdas}/${var.lambda_function_name}/${var.lambda_function_name}.zip"
+  function_name = "${local.env}_${var.lambda_function_name}"
   role          = aws_iam_role.iam_role_lambda.arn
   handler       = "index.handler"
 
@@ -38,6 +42,43 @@ resource "aws_lambda_function" "test_lambda" {
       TRANSACTION_TABLE_NAME = "mesh-app.plaid.transactions.${local.env}"
     }
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.example,
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/${local.env}_${var.lambda_function_name}"
+  retention_in_days = 14
+}
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_role_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
 }
 
 output "function_name" {
--- serverless/terraform/services/lambdas/crudPlaid/locals.tf
+++ serverless/terraform/services/lambdas/crudPlaid/locals.tf
@@ -1,5 +1,4 @@
 locals {
   env="test"
-  lambda_crudPlaid="crudPlaid"
   path_to_lambdas = "../../lambdas"
 }
