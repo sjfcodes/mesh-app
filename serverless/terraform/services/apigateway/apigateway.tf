513fa138c39166030010ffcca5fd6709c8a156d8
sjfox <sjfcodes@gmail.com>
2023-04-25 20:01:16 -0700

    update apigw service

--- serverless/terraform/services/apigateway/apigateway.tf
+++ serverless/terraform/services/apigateway/apigateway.tf
@@ -36,17 +36,17 @@ resource "aws_api_gateway_authorizer" "this" {
   provider_arns = data.aws_cognito_user_pools.this.arns
 }
 
-resource "aws_api_gateway_method" "path1_get" {
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_resource.path1.id
-  http_method   = "GET"
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.this.id
+# resource "aws_api_gateway_method" "path1_get" {
+#   rest_api_id   = aws_api_gateway_rest_api.this.id
+#   resource_id   = aws_api_gateway_resource.path1.id
+#   http_method   = "GET"
+#   authorization = "COGNITO_USER_POOLS"
+#   authorizer_id = aws_api_gateway_authorizer.this.id
 
-  request_parameters = {
-    "method.request.path.proxy" = true
-  }
-}
+#   request_parameters = {
+#     "method.request.path.proxy" = true
+#   }
+# }
 
 resource "aws_api_gateway_method" "path1_post" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
@@ -60,7 +60,7 @@ resource "aws_api_gateway_method" "path1_post" {
   }
 }
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_integration" "this" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   resource_id             = aws_api_gateway_resource.path1.id
   http_method             = aws_api_gateway_method.path1_post.http_method
@@ -79,4 +79,33 @@ resource "aws_lambda_permission" "apigw_lambda" {
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.this.id}/*/${aws_api_gateway_method.path1_post.http_method}${aws_api_gateway_resource.path1.path}"
+}
+
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.path1.id,
+      aws_api_gateway_method.path1_post.id ,
+      aws_api_gateway_integration.this.id ,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "this" {
+  deployment_id = aws_api_gateway_deployment.this.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  stage_name    = "test"
 }
