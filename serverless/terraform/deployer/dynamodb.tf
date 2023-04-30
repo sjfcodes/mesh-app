72f0b3912d9c57b089e2185ca06b0b6b811a612d
sjfox <sjfcodes@gmail.com>
2023-04-30 13:20:21 -0700

    update deployer permissions

--- serverless/terraform/deployer/dynamodb.tf
+++ serverless/terraform/deployer/dynamodb.tf
@@ -24,6 +24,40 @@ resource "aws_iam_policy" "dynamodb" {
           "arn:aws:dynamodb:${local.region}:${local.account_id}:table/prod_mesh-app_tfstate_lock",
         ]
       },
+      {
+        # Table
+        Effect = "Allow"
+        Action = [
+          "dynamodb:CreateTable",
+          "dynamodb:DeleteTable",
+          "dynamodb:DescribeContinuousBackups",
+          "dynamodb:DescribeTable",
+          "dynamodb:DescribeTimeToLive",
+          "dynamodb:TagResource",
+          "dynamodb:ListTagsOfResource",
+        ]
+        Resource = [
+          "arn:aws:dynamodb:${local.region}:${local.account_id}:table/mesh-app.test.users",
+          "arn:aws:dynamodb:${local.region}:${local.account_id}:table/mesh-app.prod.users",
+          "arn:aws:dynamodb:${local.region}:${local.account_id}:table/mesh-app.test.transactions",
+          "arn:aws:dynamodb:${local.region}:${local.account_id}:table/mesh-app.prod.transactions",
+        ]
+      },
+      {
+        # Table Items
+        Effect = "Allow"
+        Action = [
+          "dynamodb:DeleteItem",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+        ]
+        Resource = [
+          "arn:aws:dynamodb:${local.region}:${local.account_id}:table/mesh-app.test.users",
+          "arn:aws:dynamodb:${local.region}:${local.account_id}:table/mesh-app.prod.users",
+          "arn:aws:dynamodb:${local.region}:${local.account_id}:table/mesh-app.test.transactions",
+          "arn:aws:dynamodb:${local.region}:${local.account_id}:table/mesh-app.prod.transactions",
+        ]
+      },
     ]
   })
 
