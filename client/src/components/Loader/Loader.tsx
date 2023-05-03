6cafe77ca87958b3bb35f3d180f7148cd9181511
sjfox <sjfcodes@gmail.com>
2023-05-03 02:01:01 -0700

    center loader

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -2,7 +2,7 @@ import './style.scss';
 
 export default function Loader() {
   return (
-    <>
+    <div className="ma-loader-wrapper">
       <div className="lds-roller">
         <div></div>
         <div></div>
@@ -13,6 +13,6 @@ export default function Loader() {
         <div></div>
         <div></div>
       </div>
-    </>
+    </div>
   );
 }
