5a91550408af0974a9c8bb651a3706ce7fdd2142
sjfox <sjfcodes@gmail.com>
2023-05-03 22:38:52 -0700

    move formatted transactions to separate hook

--- client/src/components/TransactionTimeline.tsx
+++ client/src/components/TransactionTimeline.tsx
@@ -1,83 +1,38 @@
-import { useMemo } from 'react';
+import { useEffect } from 'react';
 import usePlaidItems from '../hooks/usePlaidItems';
 import useTransactions from '../hooks/usePlaidTransactions';
-import { PlaidTransactionType, TransactionType } from '../types';
 
-import { currencyFilter } from '../util';
 import TransactionsTable from './TransactionTable/TransactionsTable';
 import Loader from './Loader/Loader';
+import useFormattedTransactions from '../hooks/useFormattedTransactions';
 
 export default function TransactionTimeline() {
-  const { allTransactions } = useTransactions();
-  const { allAccounts } = usePlaidItems();
+  const { allAccounts, isLoading: accountsLoading } = usePlaidItems();
+  const { loadingMap, getItemAccountTransactions } = useTransactions();
+  const { formattedTxs } = useFormattedTransactions();
 
-  const formattedTxs: TransactionType[] = useMemo(() => {
-    if (!allTransactions.length) return [];
-    const processed = [] as TransactionType[];
+  useEffect(() => {
+    const hasLoaded = Object.keys(loadingMap);
+    const needsLoading = allAccounts.filter(
+      ({ id }) => !hasLoaded.includes(id)
+    );
 
-    const processTx = (txData: TransactionType) => {
-      const { transaction: tx } = txData;
-      if (tx?.name?.includes('Online Banking Transfer')) {
-        if (tx.name.includes('To')) {
-          /**
-           * if tx name contains "To" as in transfer "To xxxxxxxx<mask>"
-           * search for account with matching 4 digit mask
-           * if account is found, overwrite name with formatted name.
-           *
-           * if tx name contains "from" as in transfer "from xxxxxxxx<mask>"
-           * search for account with matching 4 digit mask
-           * if account is found, return null as tx was formatted and logged already
-           */
-          const source = allAccounts.filter(
-            (account) => account.id === tx.account_id
-          )[0];
+    needsLoading.map(({ item_id: itemId, id: accountId }) => {
+      console.log('loading', accountId);
+      return getItemAccountTransactions(itemId, accountId);
+    });
+  }, []);
 
-          const mask = tx.name
-            .split(' ')
-            .filter((str) => {
-              const last4Chars = str.slice(-4);
-              return !isNaN(parseInt(last4Chars));
-            })[0]
-            .slice(-4);
-
-          const target = allAccounts.filter((account) => {
-            return account.mask === mask;
-          })[0];
-          if (target) {
-            const sourceLabel = `[${source.subtype}-${source.mask}]`;
-            const targetLabel = `[${target.subtype}-${target.mask}]`;
-            tx.name = `Transfer ${currencyFilter(
-              Math.abs(tx.amount)
-            )} from ${sourceLabel} to ${targetLabel}`;
-            tx.amount = 0;
-          }
-        } else if (tx.name?.includes('from')) {
-          const mask = tx.name.split(' ')[4].slice(-4);
-          const source = allAccounts.filter(
-            (account) => account.mask === mask
-          )[0];
-
-          if (source) {
-            return null;
-          }
-        }
-      }
-      processed.push({
-        ...txData,
-        transaction: tx as PlaidTransactionType,
-      });
-    };
-
-    allTransactions.forEach(processTx);
-    return processed;
-  }, [allAccounts, allTransactions]);
+  if (accountsLoading) {
+    return <Loader />;
+  }
 
   return (
     <main>
       {formattedTxs.length ? (
         <TransactionsTable transactions={formattedTxs} fullHeight />
       ) : (
-        <Loader />
+        'no txs loaded yet'
       )}
     </main>
   );
---
+++ client/src/hooks/useFormattedTransactions.tsx
@@ -0,0 +1,75 @@
+import { useMemo } from 'react';
+import { PlaidTransactionType, TransactionType } from '../types';
+import useTransactions from './usePlaidTransactions';
+import usePlaidItems from './usePlaidItems';
+import { currencyFilter } from '../util';
+
+const useFormattedTransactions = () => {
+  const { allAccounts } = usePlaidItems();
+  const { allTransactions } = useTransactions();
+  const formattedTxs: TransactionType[] = useMemo(() => {
+    if (!allTransactions.length) return [];
+
+    const processed = [] as TransactionType[];
+
+    const processTx = (txData: TransactionType) => {
+      const { transaction: tx } = txData;
+      if (tx?.name?.includes('Online Banking Transfer')) {
+        if (tx.name.includes('To')) {
+          /**
+           * if tx name contains "To" as in transfer "To xxxxxxxx<mask>"
+           * search for account with matching 4 digit mask
+           * if account is found, overwrite name with formatted name.
+           *
+           * if tx name contains "from" as in transfer "from xxxxxxxx<mask>"
+           * search for account with matching 4 digit mask
+           * if account is found, return null as tx was formatted and logged already
+           */
+          const source = allAccounts.filter(
+            (account) => account.id === tx.account_id
+          )[0];
+
+          const mask = tx.name
+            .split(' ')
+            .filter((str) => {
+              const last4Chars = str.slice(-4);
+              return !isNaN(parseInt(last4Chars));
+            })[0]
+            .slice(-4);
+
+          const target = allAccounts.filter((account) => {
+            return account.mask === mask;
+          })[0];
+          if (target) {
+            const sourceLabel = `[${source.subtype}-${source.mask}]`;
+            const targetLabel = `[${target.subtype}-${target.mask}]`;
+            tx.name = `Transfer ${currencyFilter(
+              Math.abs(tx.amount)
+            )} from ${sourceLabel} to ${targetLabel}`;
+            tx.amount = 0;
+          }
+        } else if (tx.name?.includes('from')) {
+          const mask = tx.name.split(' ')[4].slice(-4);
+          const source = allAccounts.filter(
+            (account) => account.mask === mask
+          )[0];
+
+          if (source) {
+            return null;
+          }
+        }
+      }
+      processed.push({
+        ...txData,
+        transaction: tx as PlaidTransactionType,
+      });
+    };
+
+    allTransactions.forEach(processTx);
+    return processed;
+  }, [allAccounts, allTransactions]);
+
+  return { formattedTxs };
+};
+
+export default useFormattedTransactions;
