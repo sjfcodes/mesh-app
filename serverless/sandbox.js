4dd9dea1a77460d1d3528b0957e72daba32ec94d
sjfox <sjfcodes@gmail.com>
2023-05-07 12:35:44 -0700

    update imports, comments

--- serverless/sandbox.js
+++ serverless/sandbox.js
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
-import { handler as plaidHandler } from './lambdas/plaid/index.js';
-import config from './lambdas/plaid/utils/config.js';
+import { handler as plaidHandler } from './lambdas/plaid/src/index.js';
+import config from './lambdas/plaid/src/utils/config.js';
 import { mockApiGwRequestTransformations } from './test/utils/helpers.js';
 dotenv.config();
 
@@ -29,5 +29,5 @@ const getBalancesByAccountId = async () => {
 (async () => {
   const response = await getItems();
   console.log('sbx response:', response);
-  console.log(JSON.parse(response.body));
+  console.log(JSON.parse(response.body).data);
 })();
